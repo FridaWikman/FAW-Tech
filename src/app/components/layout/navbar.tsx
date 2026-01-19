5d8852b026877e21bc827cd3ba3f54320dcc5fda
Frida W <frida.wikman@hotmail.com>
2026-01-19 11:33:16 +0100

    Icons installed. Hamburger menu and foooter improved

--- src/app/components/layout/navbar.tsx
+++ src/app/components/layout/navbar.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Link from "next/link";
 import UiButton from "../ui/UiButton";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showBurger, setShowBurger] = useState(false);
+
   const links = [
     // { name: "Projekt", to: "/projects" },
     { name: "Tjänster", to: "/services" },
@@ -17,7 +22,7 @@ const Navbar = () => {
             FAW Tech
           </Link>
         </div>
-        <div className="flex justify-end gap-5 text-gray-400 hover:cursor-pointer">
+        <div className="hidden md:flex justify-end gap-5 text-gray-400 hover:cursor-pointer">
           {links.map((link, index) => {
             return (
               <Link href={link.to} key={index}>
@@ -26,9 +31,54 @@ const Navbar = () => {
             );
           })}
         </div>
-        <div>
+        <div className="hidden md:flex">
           <UiButton buttonName="Få offert" />
         </div>
+
+        <div className="flex md:hidden justify-end">
+          {!showBurger && (
+            <button
+              className=" cursor-pointer"
+              onClick={() => setShowBurger(true)}
+              aria-label="Öppna meny"
+            >
+              <Icon icon="material-symbols:menu" width="32" height="32" />
+            </button>
+          )}
+        </div>
+        {showBurger && (
+          <div
+            className={`fixed top-0 right-0 h-full w-[50%] sm:w-[30%] bg-theme-white shadow-lg transition-transform duration-300 z-60 ${
+              showBurger ? "translate-x-0" : "translate-x-full"
+            }`}
+          >
+            <div className="relative h-full w-full px-6 pt-6 flex flex-col items-end">
+              <button
+                onClick={() => setShowBurger(false)}
+                className="mb-8 cursor-pointer"
+                aria-label="Stäng meny"
+              >
+                <Icon
+                  icon="material-symbols:close-rounded"
+                  width="32"
+                  height="32"
+                />
+              </button>
+              <nav className="flex flex-col items-end gap-6 text-2xl w-full">
+                {links.map((link, index) => (
+                  <Link
+                    key={index}
+                    href={link.to}
+                    onClick={() => setShowBurger(false)}
+                    className="accent-hover transition-colors duration-200"
+                  >
+                    {link.name}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
